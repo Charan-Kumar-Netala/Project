9b0c842028bb8f0bd964015b022b7c8c346842cb
Charan-Kumar_Netala <charankumarnetala@gmail.com>
2022-07-17 12:56:51 +0530

    updated app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask,request,redirect,render_template
 from flask_pymongo import PyMongo
 
 app = Flask(__name__)
-app.config["MONGO_URI"] = 'mongodb+srv://Charan:Ronaldo97@cluster0.mt15p.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+app.config["MONGO_URI"] = 'mongodb+srv://<username>:<password>@cluster0.mt15p.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
 mongo = PyMongo(app)
 
 db_operations = mongo.db.users # creating Collection ('db' is Database)
